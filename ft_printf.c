7988fd34685a7806130a6b71b931fe473524f9f4
Svyatoslav Krasin <skrasin@e1z3r2p21.42.us.org>
2020-02-12 22:12:42 -0800

    return from vacation

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/25 10:30:46 by svet              #+#    #+#             */
-/*   Updated: 2019/12/28 13:35:44 by svet             ###   ########.fr       */
+/*   Updated: 2020/01/06 14:39:41 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int		ft_printf(const char *restrict format, ...)
 {
 	va_list arg;
-	int done;
+	int		done;
 
 	va_start(arg, format);
 	done = ft_vprintf(STDOUT_FILENO, format, arg);
--- ft_vprintf.c
+++ ft_vprintf.c
@@ -3,28 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vprintf.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/25 11:04:28 by svet              #+#    #+#             */
-/*   Updated: 2020/01/01 16:23:33 by svet             ###   ########.fr       */
+/*   Updated: 2020/01/06 18:15:06 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <errno.h>
 
-int			ft_vprintf(int fd, const char *restrict format, va_list ap)
+int	ft_vprintf(int fd, const char *restrict format, va_list ap)
 {
-	va_list ap_save;
-	int save_errno = errno;
+	va_list	ap_save;
+	int		save_errno;
 
+	save_errno = errno;
 	if (fd < 0 || format == NULL)
 		return (0);
 	while (*format != 0)
 	{
-		if (*format != '%')
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break ;
+			if (*format == 'd' || *format == 'i')
+				ft_putnbr(ft_atoi(va_arg(ap_save, int)));
+		}
+		else
 			ft_putchar(*format);
-		format++;
 	}
 	return (1);
 }
