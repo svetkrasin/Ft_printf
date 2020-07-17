ed56c577421e11ecfe3c006be80504cdcd2ed6f6
Sviatoslav <20727358+svetkrasin@users.noreply.github.com>
2020-07-17 12:51:46 -0700

    started implementing getters logic

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/01 16:16:50 by svet              #+#    #+#             */
-/*   Updated: 2020/07/10 22:35:47 by svet             ###   ########.fr       */
+/*   Updated: 2020/07/17 12:35:02 by svet             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	main(void)
 	char str[31] = "-123456789 This is test";
 	char *ptr;
 	unsigned long ret;
+	unsigned long max = NL_ARGMAX;
 	// size_t l = INT_MAX;
 	// int min = INT_MIN;
 	//double a = -1;
@@ -36,7 +37,7 @@ int	main(void)
    //printf("\n%zu %zu %zu %zu\n", sizeof(unsigned int), sizeof(long), sizeof(long double), sizeof(void *));
    //printf("\n%10$d %9$d %8$d %7$d %6$d %5$d %4$d %3$d %2$d %1$d %11$d %12$d\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, NL_ARGMAX);
 	//#error "HELLO "
-	printf("%1$*0$d", 5, 4);
+	printf("%9$lu", max, max, max);
 	//printf("%1$*2$d", 5, 10);
 	//long a = INT_MAX;
 	//++a;
